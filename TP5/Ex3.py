e18764bd84c2bcf2f05a6afd672d3b3ef7192fa8
Vasla13 <148783329+Vasla13@users.noreply.github.com>
2023-12-08 08:10:58 +0000

    ex3

--- TP5/Ex3.py
+++ TP5/Ex3.py
@@ -1,19 +1,18 @@
-import math
+def est_palindrome(chaine):
+    # Retire les caractères non alphabétiques et convertit en minuscules
+    chaine_epuree = ''.join(c.lower() for c in chaine if c.isalpha())
 
-# Les notes des 5 étudiants
-notes = [11.8, 2.8, 3, 2.2, 4.7]
+    # Teste si la chaîne épurée est un palindrome
+    if chaine_epuree == chaine_epuree[::-1]:
+        return True
+    else:
+        return False
 
-# Calcul de la moyenne
-moyenne = sum(notes) / len(notes)
+# Demande à l'utilisateur d'entrer une chaîne de caractères
+entree_utilisateur = input("Entrez un mot ou une phrase : ")
 
-# Calcul de la somme des carrés des écarts par rapport à la moyenne
-somme_carres_ecarts = sum((x - moyenne) ** 2 for x in notes)
-
-# Calcul de la variance
-variance = somme_carres_ecarts / len(notes)
-
-# Calcul de l'écart type en prenant la racine carrée de la variance
-ecart_type = math.sqrt(variance)
-
-print(f"L'écart type est : {ecart_type}")
-
+# Vérifie si la chaîne est un palindrome et affiche le résultat
+if est_palindrome(entree_utilisateur):
+    print("C'est un palindrome !")
+else:
+    print("Ce n'est pas un palindrome.")
